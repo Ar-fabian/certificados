cf5e7e3c823f35b289fb9f9be4b3c4d084a55596
Fabian Alvarado <fabianf_1704@hotmail.com>
2022-07-30 07:02:25 -0500

    img de screen modificada

--- src/certificates/components/screen/Screen.jsx
+++ src/certificates/components/screen/Screen.jsx
@@ -3,7 +3,7 @@ export const Screen = ({ course }) => {
   return (
     <img
      className="screen__img" 
-     src={`/img/${course.course}.jpg`}
+     src={`img/${course.course}.jpg`}
      alt="" />
   )
 }
