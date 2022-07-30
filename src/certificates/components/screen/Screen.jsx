fc631d0ab4845d0d76426ce05da829fbbca986be
Fabian Alvarado <fabianf_1704@hotmail.com>
2022-07-30 06:54:03 -0500

    rutas de las imagenes modificadas

--- src/certificates/components/screen/Screen.jsx
+++ src/certificates/components/screen/Screen.jsx
@@ -3,7 +3,7 @@ export const Screen = ({ course }) => {
   return (
     <img
      className="screen__img" 
-     src={`/src/assets/img/${course.course}.jpg`}
+     src={`/img/${course.course}.jpg`}
      alt="" />
   )
 }
